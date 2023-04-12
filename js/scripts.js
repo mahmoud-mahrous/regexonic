4356b6256eac1baae30c8915d5612371a0548f39
mahmoud mahrous <76485154+mahmoud-mahrous@users.noreply.github.com>
2023-04-12 06:19:27 +0200

    update 2

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,48 @@ window.onscroll = function () {
 
 window.onload = function () {
 	scrollFunction();
+	/* Filter - Isotope */
+const gridCheck = document.querySelector('.grid');
+
+if (gridCheck !== null) { 
+	// init Isotope
+	var iso = new Isotope('.grid', {
+		itemSelector: '.element-item',
+		layoutMode: 'fitRows'
+	});
+
+	// bind filter button click
+	var filtersElem = document.querySelector('.filters-button-group');
+	filtersElem.addEventListener( 'click', function( event ) {
+		// only work with buttons
+		if ( !matchesSelector( event.target, 'button' ) )  {
+			return;
+		}
+		var filterValue = event.target.getAttribute('data-filter');
+		// use matching filter function
+		iso.arrange({ filter: filterValue });
+	});
+	
+	// change is-checked class on buttons
+	var buttonGroups = document.querySelectorAll('.button-group');
+	for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+		var buttonGroup = buttonGroups[i];
+		radioButtonGroup( buttonGroup );
+	}
+	
+	function radioButtonGroup( buttonGroup ) {
+		buttonGroup.addEventListener( 'click', function( event ) {
+			// only work with buttons
+			if ( !matchesSelector( event.target, 'button' ) )  {
+				return;
+			}
+			buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
+			event.target.classList.add('is-checked');
+		});
+	}
+}
+
+
 };
 
 function scrollFunction() {
@@ -175,47 +217,6 @@ if (checkVideoModal !== null) {
 	})
 }
 
-
-/* Filter - Isotope */
-const gridCheck = document.querySelector('.grid');
-
-if (gridCheck !== null) { 
-	// init Isotope
-	var iso = new Isotope('.grid', {
-		itemSelector: '.element-item',
-		layoutMode: 'fitRows'
-	});
-
-	// bind filter button click
-	var filtersElem = document.querySelector('.filters-button-group');
-	filtersElem.addEventListener( 'click', function( event ) {
-		// only work with buttons
-		if ( !matchesSelector( event.target, 'button' ) )  {
-			return;
-		}
-		var filterValue = event.target.getAttribute('data-filter');
-		// use matching filter function
-		iso.arrange({ filter: filterValue });
-	});
-	
-	// change is-checked class on buttons
-	var buttonGroups = document.querySelectorAll('.button-group');
-	for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
-		var buttonGroup = buttonGroups[i];
-		radioButtonGroup( buttonGroup );
-	}
-	
-	function radioButtonGroup( buttonGroup ) {
-		buttonGroup.addEventListener( 'click', function( event ) {
-			// only work with buttons
-			if ( !matchesSelector( event.target, 'button' ) )  {
-				return;
-			}
-			buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
-			event.target.classList.add('is-checked');
-		});
-	}
-}
 
 
 /* Back To Top Button */
